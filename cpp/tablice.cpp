13ae36e5b16c40481bea68fd41b2e521925eb9c9
sleepinthegardennn <weronika.rawska137@gmail.com>
2018-01-18 11:53:52 +0100

    16.01.2018

--- cpp/tablice.cpp
+++ cpp/tablice.cpp
@@ -5,9 +5,24 @@
 
 
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 
 using namespace std;
 
+
+void wypelnij2W(int**tab, int w, int k) {
+    srand(time(NULL));
+    for(int i=0; i<w; i++){
+        for(int j=0; j<k; j++){
+            //cout << i << j << endl;
+            tab[i][j] = rand() %11;
+            cout << setw(4) << tab[i][j];
+        }
+        cout << endl;
+    }
+    
+}
 void wprowadzDane(int *t, int ile) { 
     for(int i=0; i <ile; i++) {
         cout << "Podaj liczbę";
@@ -35,11 +50,36 @@ int tab1W() {
     }
     return 0;
 }
- 
- 
+
+int tab2W(){
+    int w, k, i;
+    cout << "Ile wierszy i kolumn?";
+    cin >> w >> k;
+    int **tab;
+    
+    try{
+       tab = new int*[w];
+    } catch(bad_alloc) {
+        cout << "Za mało pamieci!";
+        return 1;
+    }
+    for(i = 0; i < w; i++) {
+        try{
+           tab[i] = new int[k];
+        } catch(bad_alloc) {
+            cout << "Za mało pamieci!";
+            return 1;
+        }
+    }
+    wypelnij2W(tab, w, k);
+            return 0;
+}
+
+
 int main(int argc, char **argv)
 {
-	tab1W();
+    tab2W();
+	//tab1W();
 	return 0;
 }
 
