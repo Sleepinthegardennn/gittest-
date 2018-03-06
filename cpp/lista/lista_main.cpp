4b66ac8a7985afdb125993afbeb2df266f4c9036
sleepinthegardennn <weronika.rawska137@gmail.com>
2018-03-06 14:20:35 +0100

    06

---
+++ cpp/lista/lista_main.cpp
@@ -0,0 +1,50 @@
+/*
+ * lista_main.cpp
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include "lista.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+
+{
+    int a, b;
+    cout << "Podaj numer miejsca i liczbę: " << endl;
+    cin >> a >> b;
+
+{	
+	int a, b;
+	cout << "Na którym miejscu listy wstawić wartość i jaką wartość? ";
+	cin >> a >> b;
+ 
+    Lista lista;
+    lista.Dodaj(1);
+    lista.Dodaj(3);
+    lista.Dodaj(5);
+    lista.Dodaj(7);
+    lista.Dodaj(9);
+    lista.Wyswietl();
+    lista.Usun();
+    cout << endl;
+
+    lista.Wstaw(a, b);
+
+    lista.Wyswietl();
+    lista.Dodaj(2);
+    lista.Dodaj(4);
+    lista.Dodaj(6);
+    lista.Dodaj(8);
+    lista.Dodaj(10);
+    lista.Wstaw(a, b);
+    cout << endl;
+
+    lista.Wyswietl();
+	
+	return 0;
+}
+}
